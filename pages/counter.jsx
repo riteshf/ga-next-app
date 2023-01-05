ebf52ef714c262adfb8ad2e98281a0eeae68f156
Ritesh Firodiya <ritesh.firodiya@gmail.com>
2023-01-05 22:55:13 +0530

    deploy on gh and vercel

--- pages/counter.jsx
+++ pages/counter.jsx
@@ -1,7 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Counter = () => {
-  return <h1>Counter 1</h1>;
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <h1>Counter: {count}</h1>
+      <div>
+        <button>INC</button>
+        <button>DEC</button>
+      </div>
+    </div>
+  );
 };
 
 export default Counter;
